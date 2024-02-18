94a20bd167afccc457e75dcb409ef7e209036df3
MeladKarmy <meladkarmy6@gmail.com>
2024-02-18 11:51:37 +0200

    full app

---
+++ components/ShowMore.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import { ShowMoreProps } from "@/types";
+import { updateSearchParams } from "@/utils";
+import { useRouter } from "next/navigation";
+import { CustomBtn } from "@/components";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomBtn
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+export default ShowMore;
---
+++ components/index.tsx
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import CustomBtn from "./CustomBtn";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CoustemFilter from "./CoustemFilter";
+import SearchBar from "./SearchBar";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+export {
+  Hero,
+  CustomBtn,
+  Navbar,
+  Footer,
+  SearchBar,
+  CoustemFilter,
+  CarCard,
+  ShowMore,
+};
